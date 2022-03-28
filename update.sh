f393fe2cac3a207bc42d7a226245514579a84014
knu753n <knutsen.b@gmail.com>
2022-03-28 09:11:00 +0000

    added labels for actions

--- update.sh
+++ update.sh
@@ -5,8 +5,19 @@ echo "#############################"
 echo "Update script:"
 echo "Date: $(date)"
 echo "##############################"
+echo " "
+echo "Checking for updates:"
 apt update
+echo " "
+echo "Printing upgradable:"
+echo " "
 apt list --upgradable
+echo " "
+echo "Upgrading:"
 apt upgrade -y
+echo " "
+echo "Removing old files:"
 apt autoremove -y
+echo " "
+echo "Cleaning up:"
 apt autoclean -y
